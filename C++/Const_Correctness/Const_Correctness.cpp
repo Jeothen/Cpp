79515be85d6e1612f562067252cbb90a1ab302e6
jeothen <jeothen@gmail.com>
2021-01-10 14:51:14 +0900

    const correctness

--- C++/Const_Correctness/Const_Correctness.cpp
+++ C++/Const_Correctness/Const_Correctness.cpp
@@ -4,13 +4,16 @@ class C
 {
     int x = 1;
     int arr[5];
+    mutable int cache = 3;
 public:
+    C() {}
     int getX() const { return x; } // Function is const: Doesn't modify instance(x)
     int getX1() const;
     const int CgetX() const;
-    void setX(int i) { x = i; }    // can modifies instance.
-
-    // operator overload
+    void setX(int i) { x = i;};    // can modifies instance.
+    void setX1(const int& i) { x= i;}
+    void set_cash(int i) const {cache = i;}
+    // operator overload  - matching keyword
     int &operator[](size_t index) { return arr[index]; }
     const int &operator[](size_t index) const { return arr[index]; }  // const instance
 };
@@ -21,6 +24,7 @@ const int C::CgetX() const { return x; } // CgetX() type have to be const
 struct Example {
     void foo() { std::cout << "Foo" << std::endl; }
     void foo() const { std::cout << "const Foo" << std::endl; }
+//    const int x1 = getX1();    // getX1 is not const function
 };
 
 // call basic method
@@ -35,8 +39,15 @@ void const_param(const Example& a, const Example *b)
     a.foo(); b->foo();
 }
 
+void test_fun(const int& a, const int b)
+{
+    std::cout << a << " " << b << std::endl;
+    return;
+}
+
 int main() {
     C test;
+//    C::setX(k); // Cannot be accessed without create instance (not static function)
     std::cout << test.getX() << std::endl;
     test.setX(2);
     std::cout << test.getX1() << std::endl;
@@ -45,4 +56,16 @@ int main() {
     Example a; Example b;
     non_const_param(a, &b);
     const_param(a, &b);
-}
+
+    int n1 = 1, n2 = 2;
+    test_fun(n1,n2); // int passed to const int or const int& parameter
+
+    // mutable keyword
+    C ncon;
+    ncon.setX(2);
+    ncon.set_cash(2);
+
+    const C con;  // if not initialized, error occured when compile time - constructor should be implemented.
+    con.set_cash(2);
+    
+}   
--- C++/Const_Correctness/Readme.md
+++ C++/Const_Correctness/Readme.md
@@ -2,14 +2,23 @@
 
 * It is useful when the instance has no transformation or there are parameters that should not be changed by mistake.
 * If there is a `const` before parentheses in the method implementation part, the value of instance cannot be modified within the method.
+  * In the case of a const type instance, const must be declared in front of the parentheses when calling a method.
+
+* 
 
 
 
 * If instance has const, it can also using with overloading. In const, const type object is called, and in non-const, non-const  type object is called.  In the case of const, only data reading is possible, and in the case of non-const, data read/write is possible.
 
-
-
 * When a non-const value is received as a const parameter of a method, const overload is executed.
 
 
+
+* When passing int as a function parameter, the implemented function can receive const int / const int& as parameter
+* However, in the case of const type in class/struct or global function, the return value must be const type.
+
+
+
+* Member variable using mutable keyword can be changed even if intance is const type
+*  If constructor not complemented, so const type value is not initialized, error occured when compile time
 
