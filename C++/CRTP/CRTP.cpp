a4a260db1a8bda1ccfa2fb26e37075bfe5516a1f
jeothen <jeothen@gmail.com>
2021-02-01 23:56:35 +0900

    CRTP descripton and test

--- C++/CRTP/CRTP.cpp
+++ C++/CRTP/CRTP.cpp
@@ -1,18 +1,22 @@
 #include <iterator>
+#include <iostream>
+#include <ostream>
+using namespace std;
 
 // CRTP
-// General Form 
+//---------------------General Form
 template <class T>
 struct base
 {
-    // ...
-};
-struct derived : base<derived>
-{
-    // ...
+
 };
 
-// static polymorphism
+struct derived : base<derived>
+{
+
+};
+
+//---------------------static polymorphism
 
 template <typename T>
 struct base1
@@ -34,4 +38,128 @@ struct derived1 : base1<derived1>
     static void static_sub_func();
 };
 
-//
+//---------------------object counter
+
+template <typename T>
+struct counter
+{
+    static inline int objects_created = 0;
+    static inline int objects_alive = 0;
+
+    counter()
+    {
+        ++objects_created;
+        ++objects_alive;
+    }
+
+    counter(const counter &) 
+    {
+        ++objects_created;
+        ++objects_alive;
+    }
+
+protected:
+    ~counter() // just alive count down
+    {
+        --objects_alive;
+    }
+};
+
+class X : counter<X>
+{
+public :
+    void number_alive(){
+        std::cout << objects_alive;
+    }
+};
+
+class Y : counter<Y>
+{
+    
+};
+
+//---------------------polymorphic chaining
+
+template <typename ConcretePrinter>
+class Printer
+{
+public:
+    Printer(ostream &pstream) : m_stream(pstream) {}
+
+    template <typename T>
+    ConcretePrinter &print(T &&t)
+    {
+        m_stream << t; // Hello
+        return static_cast<ConcretePrinter &>(*this);
+    }
+
+    template <typename T>
+    ConcretePrinter &println(T &&t)
+    {
+        m_stream << t << endl; // Printer!
+        return static_cast<ConcretePrinter &>(*this);
+    }
+
+private:
+    ostream &m_stream;
+};
+
+// Derived class
+class CoutPrinter : public Printer<CoutPrinter>
+{
+public:
+    CoutPrinter() : Printer(cout) {}
+
+    CoutPrinter &SetConsoleColor()
+    {
+        return *this;
+    }
+};
+
+
+//--------------------- polymorphic copy construction
+
+class AbstractShape
+{
+public:
+    virtual ~AbstractShape() = default;
+    virtual std::unique_ptr<AbstractShape> clone() const = 0;
+};
+
+// This CRTP class implements clone() for Derived
+template <typename Derived>
+class Shape : public AbstractShape
+{
+public:
+    std::unique_ptr<AbstractShape> clone() const override // base class pointer
+    {
+        return std::make_unique<Derived>(static_cast<Derived const &>(*this));  // override
+    }
+
+protected:
+    // We make clear Shape class needs to be inherited
+    Shape() = default;
+    Shape(const Shape &) = default;
+    Shape(Shape &&) = default;
+};
+
+// Every derived class inherits from CRTP class instead of abstract class
+class Square : public Shape<Square>
+{
+};
+
+class Circle : public Shape<Circle>
+{
+};
+
+int main(){
+
+    // object count
+    X create1;
+    X* test;
+    test->number_alive(); // 1
+
+    //polymorphic chaining
+    CoutPrinter().print("Hello ").SetConsoleColor().println("Printer!");
+
+}  
--- C++/CRTP/Readme.md
+++ C++/CRTP/Readme.md
@@ -8,19 +8,26 @@
 
 * General Form 
 
+  * Methods within Base Class can access Derived members using templates
 
+    
 
 * Static Polymorphism 
 
+  * The base class template is not instantiated long after the member function is declared and uses the members of the derived class within its own member function using a cast
 
+  
 
 * Object counter
+  * Enable to  search the number of objects created and destroyed for a specified class using the object counter.
 
 
 
 * Polymorphic chaining
 
+  * d
 
+    
 
 * Polymorphic copy construction
 
